b51dc70ecfaf5a6c03aff53f1c3e720ccb636a32
Noah Albright <albright.noahk@gmail.com>
2019-03-08 12:59:09 -0800

    Added audio and system variable managers

---
+++ Assets/Noah/Classes/cl_VolumeManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cl_VolumeManager : MonoBehaviour
+{
+
+    public static int GetDeviceMinVolume()
+    {
+        int streammusic = 3;
+        return deviceAudio.Call<int>("getStreamMinVolume", streammusic);
+    }
+
+    public static int GetDeviceMaxVolume()
+    {
+        int streammusic = 3;
+        return deviceAudio.Call<int>("getStreamMaxVolume", streammusic);
+    }
+
+    public static int GetDeviceVolume()
+    {
+        int streammusic = 3;
+        return deviceAudio.Call<int>("getStreamVolume", streammusic);
+    }
+
+    private static AndroidJavaObject audioManager;
+
+    private static AndroidJavaObject deviceAudio
+    {
+        get
+        {
+            if (audioManager == null)
+            {
+                AndroidJavaClass up = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                AndroidJavaObject context = up.GetStatic<AndroidJavaObject>("currentActivity");
+
+                string audioName = context.GetStatic<string>("AUDIO_SERVICE");
+
+                audioManager = context.Call<AndroidJavaObject>("getSystemService", audioName);
+            }
+            return audioManager;
+        }
+    }
+}
---
+++ Assets/Noah/Scripts/scr_SystemVariableController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_SystemVariableController : MonoBehaviour
+{
+
+    [SerializeField] static int? volume;
+    [SerializeField] static int? volume_max;
+    [SerializeField] static int? volume_min;
+
+    void Start()
+    {
+        if (volume == null)
+        {
+            volume = cl_VolumeManager.GetDeviceVolume();
+        }
+        if (volume_max == null)
+        {
+            volume_max = cl_VolumeManager.GetDeviceMaxVolume();
+        }
+        if (volume_min == null)
+        {
+            volume_min = cl_VolumeManager.GetDeviceMinVolume();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        volume = cl_VolumeManager.GetDeviceVolume();
+        volume_max = cl_VolumeManager.GetDeviceMaxVolume();
+        volume_min = cl_VolumeManager.GetDeviceMinVolume();
+    }
+
+    // Properties
+
+    public int Volume
+    {
+        get
+        {
+            if (volume == null)
+            {
+                volume = cl_VolumeManager.GetDeviceVolume();
+            }
+            return (int)volume;
+        }
+    }
+
+    public int Volume_Max
+    {
+        get
+        {
+            if (volume_max == null)
+            {
+                volume_max = cl_VolumeManager.GetDeviceMaxVolume();
+            }
+            return (int)volume;
+        }
+    }
+
+    public int VolumeMin
+    {
+        get
+        {
+            if (volume_min == null)
+            {
+                volume_min = cl_VolumeManager.GetDeviceMinVolume();
+            }
+            return (int)volume;
+        }
+    }
+}
