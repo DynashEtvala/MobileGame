023d323c28e20cdd87d1b9e339b1dd8ba1686f5a
CarterPatterson <42452101+CarterPatterson@users.noreply.github.com>
2019-03-14 14:52:07 -0700

    Repo fix
    Had issues connecting to branch. Fixed it somehow. Also worked on AudioManager, Gyro input

---
+++ Assets/Carter/Scripts/AudioManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.playOnAwake = s.playOnAwake;
+            s.source.mute = s.mute;
+        }
+    }
+
+    private void Start()
+    {
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found!");
+            return;
+        }
+        else
+        {
+            s.source.Play();
+        }
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found!");
+            return;
+        }
+        else
+        {
+            s.source.Stop();
+        }
+    }
+
+    public void Mute(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found!");
+            return;
+        }
+        else
+        {
+            s.source.mute = !s.source.mute;
+        }
+    }
+}
---
+++ Assets/Carter/Scripts/GyroControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour {
+
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    private GameObject cameraContainer;
+    private Quaternion rotatation;
+
+	void Start () {
+        cameraContainer = new GameObject("CameraContainer");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyroEnabled = EnableGyro();
+	}
+
+    void Update()
+    {
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rotatation;
+        }
+    }
+
+    private bool EnableGyro() {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rotatation = new Quaternion(0f, 0f, 1f, 0f);
+            return true;
+        }
+        return false;
+    }
+}
