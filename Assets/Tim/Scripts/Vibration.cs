06ed5577011b6d3805fdd4a84871b47a6d98aa9b
Tim(Shadow) <timsnedden138@gmail.com>
2019-03-14 14:17:41 -0700

    Merge branch 'Tim'

---
+++ Assets/Tim/Scripts/Vibration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vibration : MonoBehaviour {
+
+    private int randVal;
+    void Start () {
+	}
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+      Handheld.Vibrate();
+        
+    }
+    public void OnHit()
+    {
+        randVal = Random.Range(1, 5);
+        if(randVal == 3)
+        {
+            Handheld.Vibrate();
+        }
+    }
+}
