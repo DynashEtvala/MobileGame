e034480ff884b720d51bc7cf4c1effc83afa81b2
Tim(Shadow) <timsnedden138@gmail.com>
2019-03-28 09:13:27 -0700

    Updated my SelectingObj in Master

--- Assets/Master/Scripts/scr_SelectingObj.cs
+++ Assets/Master/Scripts/scr_SelectingObj.cs
@@ -4,43 +4,94 @@ using UnityEngine;
 
 public class scr_SelectingObj : MonoBehaviour
 {
-    public int minval;
+    int minval = 1;
     public int maxval;
     public int hitval;
-    private int randVal;
-    public GameObject cube;
     public Camera cam;
+    public List<string> tagValue;
+    public List<GameObject> Compnents;
+    public int health;
     void Start()
     {
+        for (int t = 0; t < Compnents.Count; t++)
+        {
+            Compnents[t].transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
     }
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 touchPosFar = new Vector3(Input.mousePosition.x,Input.mousePosition.y,cam.farClipPlane);
+            Vector3 touchPosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.farClipPlane);
             Vector3 touchPosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane);
             Vector3 touchPosF = cam.ScreenToWorldPoint(touchPosFar);
             Vector3 touchPosN = cam.ScreenToWorldPoint(touchPosNear);
             RaycastHit hit;
 
-            if (Physics.Raycast(touchPosN,touchPosF-touchPosN,out hit))
+            if (Physics.Raycast(touchPosN, touchPosF - touchPosN, out hit))
             {
+                for (int i = 0; i < tagValue.Capacity; i++)
+                {
+                    if (hit.transform.gameObject.tag == tagValue[i] && tagValue[i] != "Fire")
+                    {
+                        switch (tagValue[i])
+                        {
+                            case "Sensor":
+                                Debug.Log("Sensors Online");
+                                hit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Random.ColorHSV());
+                                break;
+                            case "Targeting":
+                                Debug.Log("Targeting Onlne");
+                                hit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Random.ColorHSV());
+                                break;
+                            case "Health":
+                                Debug.Log("Your Health Is:" + health);
+                                hit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Random.ColorHSV());
+                                break;
+                        }
+                    }
+                    if (hit.transform.gameObject.tag == tagValue[i] && tagValue[i] == "Fire")
+                    {
+                        int randHitVal = Random.Range(minval, maxval + 1);
+                        if (hitval == randHitVal)
+                        {
+                            Debug.Log("Hit");
+                            int randDmg = Random.Range(1, 100);
+                            health = health - randDmg;
+                            int randomComp = Random.Range(0, Compnents.Count + 1);
+                            Handheld.Vibrate();
+                            if (health < 100)
+                            {
+                                if (Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color == Color.green)
+                                {
+                                    Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 0.39f, 0.0f, 1);
+                                    break;
+                                }
+                                if (Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color == new Color(1.0f, 0.39f, 0, 1))
+                                {
+                                    Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color = new Color(0.59f, 0.0f, 0, 1);
+                                    break;
+                                }
+                                if (Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color == new Color(0.59f, 0.0f, 0, 1))
+                                {
+                                    Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color = Color.black;
+                                    break;
+                                }
+                                else
+                                {
+                                    break;
+                                }
 
-                    Debug.Log("Sensor Opening");
-                    Handheld.Vibrate();
+                            }
+                        }
+                        else
+                        {
+                            Debug.Log("Miss");
+                        }
+                    }
+                }
             }
-        }
-
         }
     }
 
-        //if (Input.touchCount > 0)
-        //{
-        //    touch = Input.GetTouch(0);
-        //    Debug.DrawRay(touch.position, transform.forward);
-        //    if (Physics.Raycast(ray, out hit, 100.0f))
-        //    {
-        //        Debug.Log("HIT");
-        //    }
-        //}
+}
