923af186cd737ea5832e5745ceaaca725fd32801
Tim Snedden <spolsiondude50@gmail.com>
2019-04-02 11:50:52 -0700

    Made Changes to the Player Commands and Enemy Firing
    tweaked names and worked on functions/Lose function

--- Assets/Tim/Scripts/PlayerCommands.cs
+++ Assets/Tim/Scripts/PlayerCommands.cs
@@ -1,130 +1,185 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerCommands : MonoBehaviour
 {
     int minval = 1;
+    public int grayedComponents = 0;
     public int maxVal;
     public int hit;
     public Camera cam;
+    public List<MyItem> Components = new List<MyItem>();
     public List<string> tagValue;
-    public List<GameObject> Compnents;
+    public Score score;
     public int StartingHealth;
     public int healthRemaining; 
     void Start()
     {
-        for (int t = 0; t < Compnents.Count; t++)
+        for (int t = 0; t < Components.Count; t++)
         {
-            Compnents[t].transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            Components[t].Obj.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
         }
         healthRemaining = StartingHealth;
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Lose(healthRemaining) == false)
         {
-            Vector3 touchPosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.farClipPlane);
-            Vector3 touchPosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane);
-            Vector3 touchPosF = cam.ScreenToWorldPoint(touchPosFar);
-            Vector3 touchPosN = cam.ScreenToWorldPoint(touchPosNear);
-            RaycastHit rayHit;
-
-            if (Physics.Raycast(touchPosN, touchPosF - touchPosN, out rayHit))
+            if (Input.GetMouseButtonDown(0))
             {
-                for (int i = 0; i < tagValue.Capacity; i++)
+                Vector3 touchPosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.farClipPlane);
+                Vector3 touchPosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane);
+                Vector3 touchPosF = cam.ScreenToWorldPoint(touchPosFar);
+                Vector3 touchPosN = cam.ScreenToWorldPoint(touchPosNear);
+                RaycastHit rayHit;
+
+                if (Physics.Raycast(touchPosN, touchPosF - touchPosN, out rayHit))
                 {
-                    if (rayHit.transform.gameObject.tag == tagValue[i] && tagValue[i] != "Fire" && healthRemaining > 0)
+                    for (int i = 0; i < tagValue.Capacity; i++)
                     {
-                        switch (tagValue[i])
+                        if (rayHit.transform.gameObject.tag == tagValue[i] && tagValue[i] != "Fire" && healthRemaining > 0)
                         {
-                            case "Sensor":
-                                Debug.Log("Sensors Online");
-                                rayHit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Random.ColorHSV());
-                                break;
-                            case "Targeting":
-                                Debug.Log("Targeting Onlne");
-                                rayHit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Random.ColorHSV());
-                                break;
-                            case "Health":
-                                Debug.Log("Your Health Is:" + healthRemaining);
-                                rayHit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Random.ColorHSV());
-                                break;
-                        }
-                    }
-                    if (rayHit.transform.gameObject.tag == tagValue[i] && tagValue[i] == "Fire" && healthRemaining > 0)
-                    {
-                        int randHitVal = Random.Range(minval, maxVal);
-                        if (hit == randHitVal)
-                        {
-                            Debug.Log("Hit");
-                            healthRemaining = healthRemaining - 10;
-                            int randomComp = Random.Range(0, Compnents.Count);
-                            Debug.Log(randomComp);
-                            Handheld.Vibrate();
-                            if (healthRemaining < StartingHealth && healthRemaining != 0)
+                            switch (tagValue[i])
                             {
-                                if (Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color == Color.green)
-                                {
-                                    Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 0.39f, 0.0f, 1);
-                                    Debug.Log("100% Health");
+                                case "Sensor":
+                                    Debug.Log("Sensors Online");
+                                    rayHit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Random.ColorHSV());
                                     break;
-                                }
-                                if (Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color == new Color(1.0f, 0.39f, 0, 1))
-                                {
-                                    Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color = new Color(0.59f, 0.0f, 0, 1);
-                                    Debug.Log("50% Health");
+                                case "Targeting":
+                                    Debug.Log("Targeting Onlne");
+                                    rayHit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Random.ColorHSV());
                                     break;
-                                }
-                                if (Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color == new Color(0.59f, 0.0f, 0, 1))
-                                {
-                                    Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color = Color.black;
-                                    Debug.Log("25% Health");
+                                case "Health":
+                                    Debug.Log("Your Health Is:" + healthRemaining);
+                                    rayHit.transform.gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Random.ColorHSV());
                                     break;
-                                }
-                                if (Compnents[randomComp].transform.gameObject.GetComponent<Renderer>().material.color == Color.black)
-                                {
-                                    Debug.Log("0% Health");
-                                    break;
-                                }
                             }
-
-                            else if(healthRemaining < 0)
+                        }
+                        if (rayHit.transform.gameObject.tag == tagValue[i] && tagValue[i] == "Fire")
+                        {
+                            int randHitVal = Random.Range(minval, maxVal);
+                            if (hit == randHitVal)
                             {
-                                Debug.Log("You Lose");
+                                Debug.Log("Hit");
+                                healthRemaining = healthRemaining - 10;
+                                int randomComp = Random.Range(0, Components.Count);
+                                Debug.Log(randomComp);
+                                Handheld.Vibrate();
+                                if (healthRemaining < StartingHealth && healthRemaining != 0)
+                                {
+                                    if (Components[randomComp].DmgTaken == 0)
+                                    {
+                                        Components[randomComp].Obj.transform.gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 0.39f, 0.0f, 1);
+                                        Components[randomComp].DmgTaken = 1;
+                                        score.scoreVal = score.scoreVal - 10;
+                                        Debug.Log("100% Health");
+                                        break;
+                                    }
+                                    if (Components[randomComp].DmgTaken == 1)
+                                    {
+                                        Components[randomComp].Obj.transform.gameObject.GetComponent<Renderer>().material.color = new Color(0.59f, 0.0f, 0, 1);
+                                        Components[randomComp].DmgTaken = 2;
+                                        score.scoreVal = score.scoreVal - 20;
+                                        Debug.Log("50% Health");
+                                        break;
+                                    }
+                                    if (Components[randomComp].DmgTaken == 2)
+                                    {
+                                        Components[randomComp].Obj.transform.gameObject.GetComponent<Renderer>().material.color = Color.black;
+                                        Components[randomComp].DmgTaken = 3;
+                                        score.scoreVal = score.scoreVal - 30;
+                                        Debug.Log("25% Health");
+                                        break;
+                                    }
+                                    if (Components[randomComp].DmgTaken == 3)
+                                    {
+                                        Components[randomComp].Obj.transform.gameObject.GetComponent<Renderer>().material.color = Color.gray;
+                                        Components[randomComp].DmgTaken = 4;
+                                        grayedComponents = grayedComponents + 1;
+                                        randomComp = Random.Range(0, Components.Count - grayedComponents);
+                                        score.scoreVal = score.scoreVal - 40;
+                                        Debug.Log("0% Health");
+                                        break;
+                                    }
+                                }
+
+                                else if (healthRemaining < 0)
+                                {
+                                    Debug.Log("You Lose");
+                                    print(score.scoreVal);
+                                    break;
+                                }
+
+                            }
+                            else
+                            {
+                                Debug.Log("Miss");
+                            }
+                        }
+                        if (rayHit.transform.gameObject.tag == tagValue[i] && tagValue[i] == "Repair")
+                        {
+                            int DamagedComp = Random.Range(0, Components.Count);
+                            if (Components[DamagedComp].DmgTaken == 1)
+                            {
+                                Components[DamagedComp].Obj.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
+                                Components[DamagedComp].DmgTaken = 0;
+                                score.scoreVal = score.scoreVal + 30;
+                                Debug.Log("100% Health");
                                 break;
                             }
+                            if (Components[DamagedComp].DmgTaken == 2)
+                            {
+                                Components[DamagedComp].Obj.transform.gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 0.39f, 0.0f, 1);
+                                Components[DamagedComp].DmgTaken = 1;
+                                score.scoreVal = score.scoreVal + 20;
+                                Debug.Log("50% Health");
+                                break;
+                            }
+                            if (Components[DamagedComp].DmgTaken == 3)
+                            {
+                                Components[DamagedComp].Obj.transform.gameObject.GetComponent<Renderer>().material.color = new Color(0.59f, 0.0f, 0, 1);
+                                Components[DamagedComp].DmgTaken = 2;
+                                score.scoreVal = score.scoreVal + 10;
 
-                        }
-                        else
-                        {
-                            Debug.Log("Miss");
-                        }
-                    }
-                    if (rayHit.transform.gameObject.tag == tagValue[i] && tagValue[i] == "Repair" && healthRemaining > 0)
-                    {
-                        int DamagedComp = Random.Range(0, Compnents.Count);
-                        if (Compnents[DamagedComp].transform.gameObject.GetComponent<Renderer>().material.color == Color.black)
-                        {
-                            Compnents[DamagedComp].transform.gameObject.GetComponent<Renderer>().material.color = new Color(0.59f, 0.0f, 0, 1);
-                            Debug.Log("25% Health");
-                            break;
-                        }
-                        if (Compnents[DamagedComp].transform.gameObject.GetComponent<Renderer>().material.color == new Color(0.59f, 0.0f, 0, 1))
-                        {
-                            Compnents[DamagedComp].transform.gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 0.39f, 0.0f, 1);
-                            Debug.Log("50% Health");
-                            break;
-                        }
-                        if (Compnents[DamagedComp].transform.gameObject.GetComponent<Renderer>().material.color == new Color(1.0f, 0.39f, 0.0f, 1))
-                        {
-                            Compnents[DamagedComp].transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
-                            Debug.Log("100% Health");
-                            break;
+                                Debug.Log("25% Health");
+                                break;
+                            }
                         }
                     }
                 }
             }
         }
+        else if(healthRemaining == 0)
+        {
+           
+        }
     }
+public bool Lose(int health)
+    {
+        if(health <= 0)
+        {
+            Debug.Log("You Lose");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+[System.Serializable]
+public class MyItem
+{
+    public GameObject Obj;
+    public int DmgTaken;
+}
+[System.Serializable]
+[HideInInspector]
+public class Score
+{
+    [HideInInspector]
+    public int scoreVal;
+    [HideInInspector]
+    public Text text;
 }
