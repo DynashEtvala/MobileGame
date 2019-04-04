b841d9dfec491b106f1401e94544b1d1e9417e57
CarterPatterson <42452101+CarterPatterson@users.noreply.github.com>
2019-04-04 12:59:09 -0700

    Ultimate Gyroness
    Wow did a lot on gyro. System is getting polished.

--- Assets/Carter/Scripts/GyroControl.cs
+++ Assets/Carter/Scripts/GyroControl.cs
@@ -6,35 +6,40 @@ public class GyroControl : MonoBehaviour {
 
     [SerializeField]
     private bool gyroEnabled;
-    private Gyroscope gyro;
-    [SerializeField]
-    private Quaternion rotatation;
-    [SerializeField]
-    float angle;
-    [SerializeField]
-    private GameObject gyroContainer;
+    private bool firstRun;
+    public float distanceToPlane;
+    private float distanceToQuad;
 
-    //Data oriented method
+
+    private GameObject gyroContainer;
     private GameObject objectManager;
+    public GameObject quad;
+    public GameObject eye;
+    public GameObject blipPrefab;
+    private List<GameObject> sectorObjectsVis;
+    public List<GameObject> blips;
+
+    private Transform eyeTransform;
+
+    private Camera cam;
+
+    private Gyroscope gyro;
+
+    private Quaternion rotatation;
+
+    private Plane hitPlane;
+
     private scr_SectorController sectorController;
     private cl_Sector currSector;
     private List<cl_SectorObject> sectorObjects;
-    private List<GameObject> sectorObjectsVis;
-    public List<GameObject> blips;
-    public GameObject blipPrefab;
-    private Plane hitPlane;
-    public float distanceToPlane;
-    public GameObject eye;
-    private Transform eyeTransform;
-    public GameObject quad;
-    private float distanceToQuad;
 
-    bool test;
-    
+    private Dictionary<GameObject, cl_SectorObject> blipToObjDict = new Dictionary<GameObject, cl_SectorObject>();
 
 	void Start () {
         gyroContainer = new GameObject("Gyro Container");
+        gyroContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
         gyroContainer.transform.position = transform.position;
+        rotatation = new Quaternion(0f, 0f, 1f, 0f);
         transform.SetParent(gyroContainer.transform);
         gyroEnabled = EnableGyro();
         objectManager = GameObject.FindGameObjectWithTag("GameManager");
@@ -44,45 +49,75 @@ public class GyroControl : MonoBehaviour {
         sectorObjectsVis = sectorController.sectorObjects;
         hitPlane = new Plane(transform.forward, transform.forward * distanceToPlane);
         eyeTransform = eye.GetComponent<Transform>();
-	}
+        cam = Camera.FindObjectOfType<Camera>();
+    }
 
     void Update()
     {
         if (gyroEnabled)
         {
             sectorObjectsVis = sectorController.sectorObjects;
-            if (!test)
+            if (!firstRun)
             {
                 for (int i = 0; i < sectorObjectsVis.Count;i++)
                 {
                     blips.Add(GameObject.Instantiate(blipPrefab));
-                    blips[i].transform.SetParent(eyeTransform);
+                    blipToObjDict.Add(blips[i], sectorObjects[i]);
                 }
-                test = true;
+                firstRun = true;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 touchPosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.farClipPlane);
+                Vector3 touchPosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane);
+                Vector3 touchPosF = cam.ScreenToWorldPoint(touchPosFar);
+                Vector3 touchPosN = cam.ScreenToWorldPoint(touchPosNear);
+                RaycastHit rayHit;
+
+                if (Physics.Raycast(touchPosN, touchPosF - touchPosN, out rayHit))
+                {
+                    Debug.Log(blipToObjDict.ContainsKey(rayHit.transform.gameObject));
+                }
             }
 
             distanceToQuad = quad.transform.position.z - transform.position.z;
-
             transform.localRotation = gyro.attitude * rotatation;
             hitPlane.SetNormalAndPosition(transform.forward, transform.forward * distanceToPlane);
-            
 
             Debug.DrawLine(transform.position, hitPlane.ClosestPointOnPlane(transform.position), Color.red, 0.0f, false);
 
             for (int i = 0; i < sectorObjectsVis.Count; i++)
             {
-                //Debug.DrawLine(transform.position, sectorObjectsVis[i].transform.position, Color.red, 0.0f, false);
-
                 float distanceToObject;
                 Ray ray = new Ray(transform.position, sectorObjectsVis[i].transform.position);
-                hitPlane.Raycast(ray, out distanceToObject);
-                Vector3 v = ray.GetPoint(distanceToObject);
 
-                eyeTransform.rotation = transform.rotation;
-                blips[i].transform.position = new Vector3(v.x, v.y, .5f);
-                eyeTransform.rotation = new Quaternion(0, 0, 0, 0);
+                if (hitPlane.Raycast(ray, out distanceToObject))
+                {
+                    Vector3 v = ray.GetPoint(distanceToObject);
+                    eyeTransform.rotation = transform.rotation;
+                    blips[i].transform.position = v;
+                    blips[i].transform.SetParent(eyeTransform, false);
+                    eyeTransform.rotation = new Quaternion(0, 0, 0, 0);
+                    blips[i].transform.SetParent(eyeTransform, true);
+                    Vector3 blipV = blips[i].transform.position;
+                    if (blipV.x < -0.5f ||
+                       blipV.x > 0.5f  ||
+                       blipV.y < -0.5f ||
+                       blipV.y > 0.5f)
+                    {
+                        blips[i].GetComponent<Renderer>().enabled = false;
+                    }
+                    else
+                    {
+                        blips[i].GetComponent<Renderer>().enabled = true;
+                    }
+                }
+                else
+                {
+                    blips[i].GetComponent<Renderer>().enabled = false;
+                }
             }
-
         }
     }
 
@@ -91,8 +126,6 @@ public class GyroControl : MonoBehaviour {
         {
             gyro = Input.gyro;
             gyro.enabled = true;
-            gyroContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
-            rotatation = new Quaternion(0f, 0f, 1f, 0f);
             return true;
         }
         return false;
